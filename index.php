078eca6117a7a34fc05cb35aaa19d4d46e2f7564
ReportDataCode <andrewcustomerhelper@gmail.com>
2021-09-07 14:32:18 +0800

    Added Form with CRUD capabilites to be added later

--- index.php
+++ index.php
@@ -3,13 +3,32 @@ require("retrieve.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DEF Company</title>
 </head>
+
 <body>
+    <h2>Create User</h2>
+    <form action="create.php" method="POST">
+        <input type="text" name="firstName" id="firstName" placeholder="Enter firstName">
+        <input type="text" name="lastName" id="lastName" placeholder="Enter LastName"><input type="text" name="email" id="email" placeholder="email"><input type="text" name="password" id="password" placeholder="password">
+        <select name="gender" id="gender">
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+
+        </select>
+        <select name="accessRole" id="accessRole">
+            <option value="">Select Role</option>
+            <option value="Client">Client</option>
+            <option value="Admin">Admin</option>
+        </select>
+        <input type="submit" value="Submit" name="Submit">
+
+    </form>
     <table>
         <tr>
             <th>First Name</th>
@@ -21,10 +40,14 @@ require("retrieve.php");
             <th>Actions</th>
         </tr>
         <?php
-        
-        while ($row = mysqli_fetch_array($sqlUser)) { 
-            ?>
+
+        while ($row = mysqli_fetch_array($sqlUser)) {
+        ?>
             <tr>
+                <td><?php echo $row['First_Name']; ?></td>
+                <td><?php echo $row['Middle_Name']; ?></td>
+                <td><?php echo $row['Last_Name']; ?></td>
+
                 <td>User 1</td>
                 <td>User Last</td>
                 <td>User Past</td>
@@ -32,11 +55,12 @@ require("retrieve.php");
                 <td>User gender</td>
                 <td>User role</td>
                 <td>Actions</td>
-                </tr>
-        <?php    
+            </tr>
+        <?php
         }
         ?>
-        
+
     </table>
 </body>
+
 </html>
