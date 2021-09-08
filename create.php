8be5ca2d222df9737ae834aced18ac44c6abf014
ReportDataCode <andrewcustomerhelper@gmail.com>
2021-09-08 15:22:48 +0800

    added boilerplate CRUD

--- create.php
+++ create.php
@@ -1,2 +1,24 @@
 <?
 require("server.php");
+require("index.php");
+
+if (isset($_POST['create'])) {
+    $firstName = $_POST['First_Name'];
+    $middleName = $_POST['Middle_Name'];
+    $lastName = $_POST['Last_Name'];
+    $age = $_POST['age'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $gender = $_POST['Gender'];
+    $accessRole = $_POST['accessRole'];
+
+    $query = "INSERT INTO students (First_Name, Middle_Name, Last_Name, Age, Gender, Access_Role) VALUES ($firstName, $middleName, $lastName, $age,  $gender, $accessRole)";
+    $sqlCreate = mysqli_query($connection, $query) OR trigger_error("Query failed SQL " . $query);
+
+    echo  "<script>alert('Successfully created!') </script>";
+    
+
+}
+else {
+    echo "<script> window.location.href='index.php'</script>";
+
+}
--- index.php
+++ index.php
@@ -14,8 +14,12 @@ require("retrieve.php");
 <body>
     <h2>Create User</h2>
     <form action="create.php" method="POST">
-        <input type="text" name="firstName" id="firstName" placeholder="Enter firstName">
-        <input type="text" name="lastName" id="lastName" placeholder="Enter LastName"><input type="text" name="email" id="email" placeholder="email"><input type="text" name="password" id="password" placeholder="password">
+        <input type="text" name="First_Name" id="First_Name" placeholder="Enter firstName">
+        <input type="text" name="Middle_Name" id="Middle_Name" placeholder="Enter MiddleName">
+        <input type="text" name="Last_Name" id="LastName" placeholder="Enter LastName">
+        <input type="text" name="email" id="email" placeholder="email">
+        <input type="password" name="password" id="password" placeholder="password">
+        
         <select name="gender" id="gender">
             <option value="Male">Male</option>
             <option value="Female">Female</option>
@@ -31,30 +35,34 @@ require("retrieve.php");
     </form>
     <table>
         <tr>
+            <th>LRN</th>
             <th>First Name</th>
+            <th>Middle Name</th>
             <th>Last Name</th>
-            <th>Email</th>
-            <th>Password</th>
+            <th>Age</th>
             <th>Gender</th>
             <th>Access Role</th>
-            <th>Actions</th>
         </tr>
         <?php
 
         while ($row = mysqli_fetch_array($sqlUser)) {
         ?>
             <tr>
+                <td><?php echo $row['Learner_Reference_Number']; ?></td>
                 <td><?php echo $row['First_Name']; ?></td>
                 <td><?php echo $row['Middle_Name']; ?></td>
                 <td><?php echo $row['Last_Name']; ?></td>
+                <td><?php echo $row['Age']; ?></td>
+                <td><?php echo $row['Gender']; ?></td>
+                
 
-                <td>User 1</td>
-                <td>User Last</td>
+                <td><?php echo $row['Access_Role'] ?></td>
+                <!-- <td>User Last</td>
                 <td>User Past</td>
                 <td>User Email</td>
                 <td>User gender</td>
                 <td>User role</td>
-                <td>Actions</td>
+                <td>Actions</td> -->
             </tr>
         <?php
         }
